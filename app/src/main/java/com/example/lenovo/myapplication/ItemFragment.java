9f6d0f18ae8c5284a21f1e20ac0c61970066327a
mohamed6996 <mohamed.ali6996@hotmail.com>
2016-11-07 23:20:01 +0200

    color

--- app/src/main/java/com/example/lenovo/myapplication/ItemFragment.java
+++ app/src/main/java/com/example/lenovo/myapplication/ItemFragment.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
@@ -16,6 +17,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -44,7 +46,6 @@ public class ItemFragment extends Fragment {
     private RecyclerView.LayoutManager mLayoutManager;
 
     private List<ItemModel> mDataSet;
-
 
     public static int type;
 
